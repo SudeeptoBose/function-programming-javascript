b1eddac638f34d6586723d1520a1c23392e32c3d
SudeeptoBose <sudeeptobose23@gmail.com>
2021-03-31 19:26:31 +0600

    Update lesson_1.js
    Functions should not depend on global variables

--- lesson_1.js
+++ lesson_1.js
@@ -1,8 +1,21 @@
 // Calculate Tax Function in ES6
+// Not a real function because it depends on global variable
 var percentValue=5
-var calculate = (value, percentValue)=>{
+var calculate = (value)=>{
     return value/100 * (100 + percentValue);
 }
-console.log(calculate(5, percentValue))
+console.log(calculate(5))
+
+// Removing dependency on global variable
+var newCalculate = (value, newPercentValue)=>{
+    return value/100 * (100 + newPercentValue);
+}
+console.log(newCalculate(5, newPercentValue = 5))
+
+
+
+
+
+
 
 
