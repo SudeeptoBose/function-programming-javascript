e3a31abfee7b1479230149af006726ad745a0e46
SudeeptoBose <sudeeptobose23@gmail.com>
2021-03-31 19:13:50 +0600

    First commit

---
+++ lesson_1.js
@@ -0,0 +1,8 @@
+// Calculate Tax Function in ES6
+var percentValue=5
+var calculate = (value, percentValue)=>{
+    return value/100 * (100 + percentValue);
+}
+console.log(calculate(5, percentValue))
+
+
