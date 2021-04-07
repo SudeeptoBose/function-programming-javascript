12766d6be2a1f990d9a55fc13e893068ffc781d5
SudeeptoBose <sudeeptobose23@gmail.com>
2021-04-07 06:49:06 +0600

    Update lesson_2.js

--- lesson_2.js
+++ lesson_2.js
@@ -1,3 +1,8 @@
-simpleFunction = () =>{
-    alert("Hello")
-}
+// Use strict is used to find out any errors before run time
+// This entire function can be converted into and arrow function
+
+"use strict";
+var simpleFn = function simpleFn() {
+return "Simple Function";
+};
+simpleFn();
