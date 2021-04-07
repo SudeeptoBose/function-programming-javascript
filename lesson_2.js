47a941595d521a23e4a3dc3d55fa5bd53061f05c
SudeeptoBose <sudeeptobose23@gmail.com>
2021-04-07 06:56:49 +0600

    Update lesson_2.js

--- lesson_2.js
+++ lesson_2.js
@@ -9,3 +9,17 @@ simpleFn();
 
 // Example of how the return & function keyword is not required
 var newSimpleFn = () => "Simple Function"
+
+// Wrap function with { } if there are multiple statements
+var anotherSimpleFn = () => { //function scope
+    if(true) {
+        let a = 1;
+        var b = 2;
+        console.log(a)
+        console.log(b)
+        } //if block scope
+    console.log(b) //function scope
+    console.log(a) //function scope
+}
+anotherSimpleFn();
+// The let keyword only creates local variables
