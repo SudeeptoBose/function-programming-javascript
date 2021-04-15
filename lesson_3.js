89c93d039dab549b1d177ae2ca7e60b34c94dc7e
SudeeptoBose <sudeeptobose23@gmail.com>
2021-04-16 01:02:54 +0600

    Update lesson_3.js

--- lesson_3.js
+++ lesson_3.js
@@ -1,8 +1,16 @@
 // Iterate using for loop
 
 var array = [1,2,3]
-for(i=0;i<array.length;i++)
+for(i=0;i<array.length;i++){
     console.log(array[i])
+}
 
 
 // Iterate using forEach
+
+const forEach = (array, fn) => {
+    let i;
+    for(i = 0; i < array.length; i++){
+        console.log(fn(array[i]))
+    }
+};
